4406e0640138014d986cabcd2f1881617ec508d8
Pratik Raj Verma <erpratikrajverma@gmail.com>
2024-07-21 16:07:31 +0530

    commit practice

---
+++ index.js
@@ -0,0 +1,11 @@
+function dev(){
+    console.log('Running in development mode');
+    console.log('Running in VS code ');
+}
+
+dev();
+
+function prod(){
+    console.log('Running in production mode');
+}
+
